893f55a07cffb8875e40ca1d678d5bf8971844da
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2022-03-08 15:43:32 +0000

    Add files via upload

---
+++ Using Service Principal to call ms graph and create a cert.ps1
@@ -0,0 +1,61 @@
+﻿#Try using MS Graph, if not use AZure AD moodules
+
+#need to sign in a SP
+#How to use this with provided credentials instead of interactive log in?
+#How to login as a SP
+#see link https://docs.microsoft.com/en-us/graph/powershell/app-only?tabs=powershell
+
+#Needed to add the platform in authentication
+
+Connect-MgGraph -Scopes 'User.Read.All'
+
+$appId = ""
+$tenantId = "" 
+$certThumbPrint = ""
+
+Connect-MgGraph -ClientID $appId -TenantId $tenantId -CertificateThumbprint $certThumbPrint #-Scopes 'User.Read.All Group.Read.All'
+
+#verfy that authenticated with app-only
+Get-MgContext
+
+# list users 
+# add app permission User.Read.All
+Get-MgUser
+
+# list groups
+Get-MgGroup
+
+######################
+#First need to create a certificate using this doc https://blogs.aaddevsup.xyz/2020/07/using-msal-net-to-perform-the-client-credentials-flow-with-a-certificate-instead-of-a-client-secret-in-a-netcore-console-appliction/
+
+$tenant_id = '' # tenant id is used for the cert name / subject name and matches the .netcore code sample that goes with this script
+$client_id = '' # client id is used for the cert name / subject name and cert password and matches the .netcore code sample that goes with this script
+$FilePath = 'C:\Users\<local path>'
+$StoreLocation = 'CurrentUser' # be aware that LocalMachine requires elevated privileges
+$expirationYears = 1
+ 
+$SubjectName = $tenant_id + '.' + $client_id
+$cert_password = $client_id
+ 
+$pfxFileName = $SubjectName + '.pfx'
+$cerFileName = $SubjectName + '.cer'
+ 
+$PfxFilePath = $FilePath + $pfxFileName
+$CerFilePath = $FilePath + $cerFileName
+ 
+$CertBeginDate = Get-Date
+$CertExpiryDate = $CertBeginDate.AddYears($expirationYears)
+$SecStringPw = ConvertTo-SecureString -String $cert_password -Force -AsPlainText
+$Cert = New-SelfSignedCertificate -DnsName $SubjectName -CertStoreLocation "cert:\$StoreLocation\My" -NotBefore $CertBeginDate -NotAfter $CertExpiryDate -KeySpec Signature
+Export-PfxCertificate -cert $Cert -FilePath $PFXFilePath -Password $SecStringPw
+Export-Certificate -cert $Cert -FilePath $CerFilePath
+
+######################
+
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" `
+
+  -Subject "CN=exampleappScriptCert" `
+
+  -KeySpec KeyExchange
+
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
---
+++ update user account enabled prop.ps1
@@ -0,0 +1,32 @@
+﻿#need to login as the app, see the case 2202170030001163
+
+$appId = "6a67243e-00d5-4df0-97e4-ff5a23182cd7"
+$tenantId = "6528db0a-dd47-4ff9-b0e7-6f5d9a39d0dc" 
+$certThumbPrint = "E3A8C914E00064C75C804B6FD61FB4431720C52B"
+
+Connect-MgGraph -ClientID $appId -TenantId $tenantId -CertificateThumbprint $certThumbPrint
+
+Get-MgContext
+
+
+#command to run
+$params = @{
+	accountEnabled = $false
+}
+
+$userId = '06bbb504-6a52-4edc-8891-d46b3c81a3ce'
+
+# A UPN can also be used as -UserId.
+Update-MgUser -UserId $userId -BodyParameter $params
+
+# How to see the status of the account enable, see in the portal user profile > Settings> block sign in yes and no
+Get-MgUser -UserId $userId -Property "displayName,givenName,postalCode,identities,accountEnabled" 
+
+
+#can revoke admin consent on port using right click
+
+#check which is need 
+#Directory.ReadWrite.All(seems this should be enough) for ordinary users
+#The app also doesn't have any directory roles
+
+#remember to close and open the this ide and reconnect
